90047ed4f999e44917ef5ca2de6d087cdf6feb2f
RuifengSu <ZRUISUX@ericsson.com>
2023-04-24 22:36:07 +0800

    1

--- web/apps/tasks/api.py
+++ web/apps/tasks/api.py
@@ -127,3 +127,101 @@ def delete_resource(res_id):
         body.update(message="未找到资源")
     body["data"] = data
     return body, body["code"]
+
+
+@resources_api.route(f'/{RESOURCE_NAME}/workflows', methods=['GET'], strict_slashes=False)
+def get_workflows():
+    body = RETURN_TEMPLATE.copy()
+    success = True
+    workflows = [class2dict(row) for row in db.session.scalars(db.select(Workflow).order_by(Task.create_time)).all()]
+    if success:
+        data = workflows
+    else:
+        data = None
+        body.update(code=404)
+        body.update(status="fail")
+        body.update(message="未找到资源")
+    body["data"] = data
+    return body, body["code"]
+
+
+@resources_api.route(f'/{RESOURCE_NAME}/workflows/<int:res_id>', methods=['GET'])
+def get_workflow(res_id):
+    body = RETURN_TEMPLATE.copy()
+    success = True
+    workflows = db.session.scalars(db.select(Workflow).where(Workflow.id == res_id)).first()
+    if workflows:
+        data = class2dict(workflows)
+    else:
+        data = None
+        body.update(code=404)
+        body.update(status="fail")
+        body.update(message="未找到资源")
+    body["data"] = data
+    return body, body["code"]
+
+
+@resources_api.route(f'/{RESOURCE_NAME}/workflows', methods=['POST'])
+def post_workflows():
+    success = True
+    body = RETURN_TEMPLATE.copy()
+    status = request.get_json().get('status')
+    owner_project = request.get_json().get('owner_project')
+    workflows = Workflow(status=status, owner_project=owner_project)
+    db.session.add(workflows)
+    db.session.commit()
+    if workflows:
+        data = class2dict(workflows)
+    else:
+        data = None
+        body.update(code=404)
+        body.update(status="fail")
+        body.update(message="未找到资源")
+    body["data"] = data
+    return body, body["code"]
+
+
+@resources_api.route(f'/{RESOURCE_NAME}/workflows/<int:res_id>', methods=['PUT'])
+def put_workflows(res_id):
+    body = RETURN_TEMPLATE.copy()
+    success = True
+    status = request.get_json().get('status')
+    owner_project = request.get_json().get('owner_project')
+    stmt = update(Workflow).where(Workflow.id == res_id).values(status=status, owner_project=owner_project)
+    db.session.execute(stmt)
+    db.session.commit()
+    if success:
+        data = {
+            'title': 'Hello World',
+            'name': 'Alice',
+            'age': res_id
+        }
+    else:
+        data = None
+        body.update(code=404)
+        body.update(status="fail")
+        body.update(message="未找到资源")
+    body["data"] = data
+    return body, body["code"]
+
+
+@resources_api.route(f'/{RESOURCE_NAME}/workflows/<int:res_id>', methods=['DELETE'])
+def delete_workflows(res_id):
+    body = RETURN_TEMPLATE.copy()
+    success = res_id
+    stmt = delete(Workflow).where(Workflow.id == res_id)
+    db.session.execute(stmt)
+    db.session.commit()
+    if success:
+        data = {
+            'title': 'Hello World',
+            'name': 'Alice',
+            'age': res_id
+        }
+    else:
+        data = None
+        body.update(code=404)
+        body.update(status="fail")
+        body.update(message="未找到资源")
+    body["data"] = data
+    return body, body["code"]
